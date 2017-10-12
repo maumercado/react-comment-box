5115d12692db745a7127b3522b42abb96f8ba4bd
Mauricio Mercado <mau@maumercado.com>
2017-10-11 22:42:03 -0700

    Made prettier comment box component

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -29,11 +29,14 @@ class CommentBox extends Component {
     render() {
         return (
             <form className="comment-box" onSubmit={this.handleSubmit}>
+                <h4>Add a comment</h4>
                 <textarea
                     value={this.state.comment}
                     onChange={this.handleChange}
                 />
-                <button action="submit">Submit Comment</button>
+                <div>
+                    <button action="submit">Submit Comment</button>
+                </div>
             </form>
         );
     }
