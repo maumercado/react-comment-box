328c9400fe693ec694407d7caa3aa1a90c6981ee
Mauricio Mercado <mau@maumercado.com>
2017-10-11 21:39:19 -0700

    Add actions tests file mirroring src structure
    Add types files to map actions types
    
    Add saveComment action with tests

---
+++ src/__tests__/actions/indext.test.js
@@ -0,0 +1,18 @@
+import { SAVE_COMMENT } from '../../actions/types';
+import { saveComment } from '../../actions';
+
+describe('actions', () => {
+    describe('saveComment', () => {
+
+        it('has the correct type', () => {
+            const action = saveComment();
+            expect(action.type).toBe(SAVE_COMMENT);
+        });
+
+        it('has the correct payload', () => {
+            const action = saveComment('new comment');
+            expect(action.payload).toBe('new comment');
+        });
+
+    });
+});
